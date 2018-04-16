703f68070c776e40e277cc85d409bfb3c63d1b64
Trung Lê <trung.tranle13@gmail.com>
2018-04-16 18:53:49 +0700

    commit

---
+++ py1.py
@@ -0,0 +1,21 @@
+a = eval(input('Nhập a'))
+b = eval(input('Nhập c'))
+c = eval(input('Nhập c'))
+d = eval(input('Nhập d'))
+
+if a >= b:
+    mx1 = a 
+else:
+    mx1 = b
+
+if c >=d
+    mx2 = c
+else:
+    mx2 = d
+
+if mx1 >= mx2
+    mx = mx1
+else: 
+    mx = mx2
+
+print('Max = ', mx)         
