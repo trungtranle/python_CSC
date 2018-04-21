2b2ef2c3aa85f3e7ef1ed456dec3b8d2ac418fea
hv <trung.tranle13@gmail.com>
2018-04-21 16:42:47 +0700

    180421

--- ex5_7.py
+++ ex5_7.py
@@ -2,14 +2,7 @@ tu = eval(input('Từ: '))
 den = eval(input('Đến: '))
 a = 1
 re = ''
-'''
-for i in range(tu,den + 1):
-    for j in range(1,11):
-        a = i * j
-        re +=  str (i) + ' x ' + str (j) + ' = ' + str (a)  + '\n'
 
-print(re)
-'''
 for j in range (1,11):
     for i in range(tu,den+1):
         a = i * j
