354cb5b656a7ff7178a48ef25f916a74d3aa565c
hv <trung.tranle13@gmail.com>
2018-05-05 13:58:22 +0700

    180505

---
+++ ex7_ex3.py
@@ -0,0 +1,13 @@
+dic = {}
+
+i = 1
+while i == 1:
+    key = input('Nhập key thứ %d: ' %(len(dic)+1))
+    val = input('Nhập val thứ %d: ' %(len(dic)+1))
+    dic[key] = val
+    i = int(input('Tiếp tục: 1, Dừng: 0 \t'))
+
+print(sorted(dic))
+
+for key in sorted(dic):
+    print(key,':', dic[key])
