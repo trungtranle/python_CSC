625a21f3b3f29d1c58c683b0a4f87eaddcfa3a11
Trung Lê <trung.tranle13@gmail.com>
2018-05-13 00:43:27 +0700

    180513

--- ex11_2_tinh_tien_nuoc.py
+++ ex11_2_tinh_tien_nuoc.py
@@ -1,14 +1,19 @@
 from ex11_2_lib_tinh_tien_nuoc import tinh_tien_nuoc
 
-try:
-    doituong = int(input('Nhập đối tượng: GB'))
-    usage = eval(input('Nhập số nước (m3): '))
-    tien_nuoc = tinh_tien_nuoc(usage=usage, doituong=doituong)
-except AssertionError as er1:
-    print('Lỗi: Không tồn tại đối tượng GB%d' % doituong)
-    a = 0
-except (NameError, SyntaxError) as er2:
-    print('Lỗi: Nhập số nước dạng số')
-else:
-    tien_nuoc = tinh_tien_nuoc(usage=usage, doituong=doituong)
-    print('Tổng tiền phải trả:', tien_nuoc, 'VNĐ')
+a = 0
+while a == 0:
+    a = 1
+    try:
+        doituong = int(input('Nhập đối tượng: GB'))
+        usage = eval(input('Nhập số nước (m3): '))
+        tien_nuoc = tinh_tien_nuoc(usage=usage, doituong=doituong)
+    except AssertionError:
+        print('Lỗi: Không tồn tại đối tượng GB%d' % doituong)
+        print('Nhập lại \n')
+        a = 0
+    except (NameError, SyntaxError):
+        print('Lỗi: Nhập số nước dạng số')
+        print('Nhập lại \n')
+        a = 0
+    else:
+        print('Tổng tiền phải trả:', tien_nuoc, 'VNĐ')
