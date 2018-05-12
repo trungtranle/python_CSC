58ae88f7cd314105d06dcb7d373335968b2ad041
hv <trung.tranle13@gmail.com>
2018-05-12 16:21:15 +0700

    180512

---
+++ ex09_char_count.py
@@ -0,0 +1,25 @@
+def dem_so_ky_tu(string):
+    cap = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'N', 'M', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    count_majus = 0
+    for i in cap:
+        count_majus += string.count(i)
+    count_minus = len(string) - count_majus
+    return count_majus, count_minus
+
+
+def dem_so_ky_tu_2(string):
+    list1 = list(string)
+    list2 = list(string.upper())
+    count_majus = 0
+    count_minus = 0
+    for i in range(0, len(string)):
+        if list1[i] == list2 [i] and list1[i] != ' ':
+            count_majus += 1
+        else:
+            count_minus += 1
+    return count_majus, count_minus
+
+s = input('Nhập chuỗi s: ')
+cap, uncap = dem_so_ky_tu_2(s)
+print('Số ký tự hoa:', cap)
+print('Số ký tự thường including space:', uncap)
---
+++ ex10_lib_txt.py
@@ -0,0 +1,38 @@
+def read_file(filename):
+    f = open(filename, 'r', encoding = 'utf-8')
+    print(f.read())
+    f.close
+
+
+def summarize_file(filename):
+    f = open(filename, 'r', encoding = 'utf-8')
+    lines = 0
+    word = 0
+    char = 0
+    for line in f:
+        lines += 1
+        word += len(line.split())
+        char += len(list(line))
+    print('---Summary---')
+    print(lines,'Lines \t', word, 'Words \t', char, 'Characters')
+    f.close()
+
+
+def write_file_once(filename):
+    f = open(filename, 'w+', encoding = 'utf-8')
+    content = input('Content: ')
+    f.write(content)
+    print('Written in', f.name)
+    f.close()
+
+
+def write_at_file_end(filename):
+    f = open(filename, 'a+', encoding = 'utf-8')
+    i = 1
+    while i == 1:
+        content = input('Content: ')
+        f.write(content)
+        i = int(input('Tiếp tục nhập: Tiếp tục: 1, Không <> 1: '))
+    print('Đã ghi nội dung vào file',filename) 
+    f.close()   
+
