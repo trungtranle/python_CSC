61edde36226572a9a8f2818190e9742762af540e
hv <trung.tranle13@gmail.com>
2018-05-19 16:51:39 +0700

    180519

--- python_nangcao/ex1_1_giai_pt_bac_nhat_lib.py
+++ python_nangcao/ex1_1_giai_pt_bac_nhat_lib.py
@@ -1,9 +1,9 @@
 class Phuong_trinh_bac_nhat(object):
 
-    def __init__(self, a , b):
+    def __init__(self, a, b):
         self.a = a
         self.b = b
-    
+
     def tinh_nghiem(self):
         if self.a == 0 and self.b != 0:
             return 'Phương trình vô nghiệm'
@@ -11,7 +11,6 @@ class Phuong_trinh_bac_nhat(object):
             return 'Phương trình vô số nghiệm'
         else:
             return -self.b/self.a
-    
+
     def in_ket_qua(self):
         print('Kết quả: ', self.tinh_nghiem())
-        
